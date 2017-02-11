7dd15c1e45bb8da086676ae157ee267a25200504
Marcelo Vilela <mvilelapardo@gmail.com>
2017-02-11 16:26:35 -0400

    Update Hello_World_LED

--- Hello_World_LED/Hello_World_LED.ino
+++ Hello_World_LED/Hello_World_LED.ino
@@ -1,7 +1,18 @@
+int led = 9;
+int brightness = 0;
+int fadeAmount = 5;
+
 void setup() {
   pinMode(12, OUTPUT);
+  pinMode(led, OUTPUT);
 }
 void loop() {
+  while(brightness <= 255){
+      analogWrite(led, brightness);
+      brightness = brightness + fadeAmount;
+      delay(30);
+  }
+  brightness = 0;
   digitalWrite(12, HIGH);
   delay(2500);
   digitalWrite(12, LOW);
